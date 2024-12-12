4fc8795b8193771d0feeaf21f10e55f918e12688
Roshan <roshan.shrestha@voxcrow.com>
2024-12-12 17:14:50 +0545

    register added

--- frontend/src/components/Reports.jsx
+++ frontend/src/components/Reports.jsx
@@ -3,43 +3,31 @@ import Sidebar from "../components/Sidebar";
 
 const Reports = () => {
     return (
-        <div >
+        <div className="d-flex">
             <Sidebar />
-            <div className="table-container">
-                <table className="styled-table">
+            <div className="flex-grow-1 p-4">
+                <h2>Reports</h2>
+                <table className="table table-striped">
                     <thead>
                         <tr>
-                            <th>Call Date</th>
+                            <th>Calldate</th>
                             <th>Agent</th>
-                            <th>Number</th>
-                            <th>Extension</th>
-                            <th>Recording</th>
                             <th>Duration</th>
-                            <th>Action</th>
+                            <th>Recording</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>
-                                <a target="_blank" rel="noopener noreferrer">
-                                    Listen
-                                </a>
-                            </td>
-                            <td>5</td>
-                            <td>
-                                <button className="action-btn">View</button>
-                            </td>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
-
-    );
+    )
 };
 
 export default Reports;
