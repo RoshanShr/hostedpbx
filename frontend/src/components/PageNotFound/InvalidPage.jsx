5fdb4bc8a4b4e7ba942bc0bdcff11ff316df8fda
Roshan <roshan.shrestha@voxcrow.com>
2024-12-20 17:02:38 +0545

    projet restructured using AuthHandler and react router

--- frontend/src/components/PageNotFound/InvalidPage.jsx
+++ frontend/src/components/PageNotFound/InvalidPage.jsx
@@ -5,8 +5,8 @@ export default function InvalidPage() {
     <div>
       <h1>Sorry, this page isn't available.</h1>
       <p>
-        The link you followed may be broken, or the page may have been removed.{" "}
-        <Link to="/">Go back to HostePBX.</Link>{" "}
+        The link you followed may be broken, or the page may have been removed.
+        <Link to="/">Go back to HostePBX.</Link>
       </p>
     </div>
   );
